04cf887eae6e7c916807340aa8bac3b0d4aea18a
ogubuike245 <ogubuike245@users.noreply.github.com>
2020-10-24 13:16:49 +0300

    yeah

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = (props) => {
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo left">
-          OGU
+          BASIC HOMES
         </Link>
 
         {links}
